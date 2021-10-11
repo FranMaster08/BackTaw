b4ca0273c56625ec2465fc69585b20fb1d5dc2ba
Francisco Javier Jimenez Cohen <franciscojavierjimenezcohen@AirdeFrancisco.fibertel.com.ar>
2021-10-11 17:00:55 -0300

    se modifica error

---
+++ src/routes/homes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { userController } = require('../controller')
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/sign', function (req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+router.get('/sigout', function (req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+
+router.get('/users',userController.getUsers);
+
+module.exports = router ;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {userController} = require('../controller')
+
 
 router.use('/',require('./homes'))
 router.use('/users',require('./users'))
