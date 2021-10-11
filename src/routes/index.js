f9f4e16803e7b71df28ce08c23a086f1bdb2a0fe
Francisco Javier Jimenez Cohen <franciscojavierjimenezcohen@AirdeFrancisco.fibertel.com.ar>
2021-10-11 16:56:30 -0300

    se agrega contenedor

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {userController} = require('../controller')
 
-router.use('/',require('./home'))
+router.use('/',require('./homes'))
 router.use('/users',require('./users'))
 
 module.exports = router;
